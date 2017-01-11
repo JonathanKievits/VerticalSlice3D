f73d4fed2abd484482b084bbb6f5b606d229e5f0
JonathanKievits <Jonathan.kievits@hotmail.nl>
2017-01-11 13:37:37 +0100

    Random map generator

---
+++ VerticalSlice3D/Assets/Scripts/Environment/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    [SerializeField]private GameObject[] tracks;
+    [SerializeField]private Transform trackSpawnerPos;
+    private float newPos;
+    public static Generator generator;
+
+    private float waitTime;
+    private GameObject track;
+
+    private void Start()
+    {
+        generator = this;
+        newPos = 2.5f;
+        waitTime = 0.1f;
+        Track();
+    }
+
+    private void Track()
+    {
+        track = Instantiate(tracks[Random.Range(0, tracks.Length)], trackSpawnerPos.position, Quaternion.identity) as GameObject;
+        Vector3 temp = trackSpawnerPos.position;
+        temp.y = 0;
+        temp.x = 0;
+        temp.z += 2.5f;
+        trackSpawnerPos.position = temp;
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Track();
+    }
+}
---
+++ VerticalSlice3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _space;
+
+	void Start ()
+    {
+        _space = 2.5f;
+	}
+	
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * _space);
+        }else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * _space);
+        }else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * _space);
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * _space);
+        } 
+	}
+}
+
